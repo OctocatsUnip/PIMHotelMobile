ca333484d7b120a7775f52bd560f52935618fd76
nicozzi <nickpizzoni@outlook.com>
2021-09-06 19:47:56 -0300

    Iniciado implementação de styled-components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Login from './src/screens/Login'
+import Login from './src/screens/Login';
+import Cadastro from './src/screens/Cadastro';
 
 export default function App() {
   return (
