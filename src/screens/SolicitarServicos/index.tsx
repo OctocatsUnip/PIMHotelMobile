2c7aa149240bd2d4731360bb438aa4918a3e7809
MileneMaciel <89492025+MileneMaciel@users.noreply.github.com>
2021-10-03 07:18:09 -0300

    Solicitar Serviços finalizada

--- src/screens/SolicitarServicos/index.tsx
+++ src/screens/SolicitarServicos/index.tsx
@@ -1,15 +1,112 @@
 import React from 'react';
+import Header from '../../components/Header';
+
 
 import {
    Container,
+   Content,
+   RowContainer,
+   TitleFirst,
+   Separator,
+   SectionContainer,
+   CheckBoxContainer,
+   CheckBox,
    Text,
+   Title,
+   ServicesContainer,
+
+   
+   
 } from './styles';
 
-export default function SolicitarServicos() {
+import { Button } from '../../components/Button';
 
+
+export default function SolicitarServicos() {
    return (
-        <Container>
-            <Text>Em produção</Text>
-        </Container>
-   );
+      <Container>
+          <Header />
+          <Content>
+         
+             <TitleFirst>Executive Prime </TitleFirst>            
+             <Separator/>               
+         
+         <SectionContainer>
+                    <Title>Serviço de quarto</Title>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Limpeza</Text>
+                    </RowContainer>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Minhas Roupas</Text>
+                    </RowContainer>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Itens Higiênicos</Text>
+                    </RowContainer>
+                </SectionContainer>
+                     
+                <SectionContainer>
+                    <Title>Comidas e Bebidas</Title>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Cardápio</Text>
+                    </RowContainer>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Reposição Frigobar</Text>
+                    </RowContainer>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Bebidas</Text>
+                    </RowContainer>
+                </SectionContainer>
+
+                <SectionContainer>
+                    <Title>Manutenção</Title>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Televisão</Text>
+                    </RowContainer>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Wi-fi e Internet</Text>
+                    </RowContainer>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Chuveiro</Text>
+                    </RowContainer>
+                    <RowContainer>
+                        <CheckBoxContainer>
+                            <CheckBox />
+                        </CheckBoxContainer>
+                        <Text>Outro</Text>
+                    </RowContainer>
+                </SectionContainer>
+               
+                <ServicesContainer>
+                <Button text="Solicitar!" />
+               </ServicesContainer>
+          </Content>
+      </Container>
+ );
 }
--- src/screens/SolicitarServicos/styles.ts
+++ src/screens/SolicitarServicos/styles.ts
@@ -1,11 +1,84 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
    flex: 1;
-   align-items: center;
-   justify-content: center;
    margin-top: ${getStatusBarHeight()}px;
 `;
 
-export const Text = styled.Text``;
+
+export const Content = styled.ScrollView.attrs({
+   showsVerticalScrollIndicator: false,
+   contentContainerStyle: {
+       paddingHorizontal: 20
+   }
+})``;
+
+export const RowContainer = styled.View`
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+export const SectionContainer = styled.View`
+    border-bottom-width: 1px;
+    padding-bottom: 20px;
+    border-color: ${({theme}) => theme.colors.gray};
+    margin-bottom: 10px;
+`;
+
+export const TitleFirst = styled.Text`
+    text-align: center;  
+    font-size:28px;
+    font-weight: bold;
+    margin-bottom:10px;
+    margin-top:30px;    
+    
+`;
+
+export const Separator = styled.View`
+    border-bottom-width: 2px;
+    width: 70%;
+    border-color: #000;
+    margin-bottom: 20px;
+     margin-left:50px;
+     margin-right:5px; 
+  
+`;
+
+export const CheckBoxContainer = styled.View`
+    height: 25px;
+    width: 25px;
+    border-width: 1px;
+    margin: 15px;
+`;
+
+export const CheckBox = styled(RectButton)`
+    height: 25px;
+    width: 25px;
+    border-width: 1px;
+`;
+
+
+export const Text = styled.Text`
+    font-family: ${({theme}) => theme.fonts.regular};
+    font-size: 17px;
+    flex-shrink: 1;
+`;
+
+export const Title = styled.Text`
+    font-family: ${({theme}) => theme.fonts.regular};
+    font-weight: bold;
+    font-size: 20px;
+`;
+
+export const ServicesContainer = styled.View`
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: flex-start;
+    margin-bottom: 30px;
+    margin-top: 20px;
+`;
