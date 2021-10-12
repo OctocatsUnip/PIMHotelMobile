5a7f171bf81cb81e8ef44c886aa8d936b26b861d
Kauhan Gomes <kauhangomes9@gmail.com>
2021-10-11 21:17:12 -0300

    Tela Atualizar Cadastro

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,6 +4,5 @@ import { TextInput } from 'react-native';
 export const Container = styled(TextInput)`
     align-items: center;
     justify-content: center;
-    background-color: red;
     width: 100%;
 `;
